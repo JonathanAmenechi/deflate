3bdbb445ca7bd2eb2ec66aa4a5fa0df38ae29627
jonamen <jonathanamenechi@gmail.com>
2018-03-17 17:06:29 -0400

    Implementation of Naive huffman coding and canonical huffman coding

--- huffman.py
+++ huffman.py
@@ -1,6 +1,8 @@
-from collections import Counter
+from collections import Counter, OrderedDict
 import heapq
+import sys
 
+is_py3 = sys.version_info.major >= 3
 
 class HuffmanNode(object):
 
@@ -13,30 +15,40 @@ class HuffmanNode(object):
     def __repr__(self):
         return "Node(Symbol:'{}',Freq:{})\n".format(self.symbol,self.freq)
 
-    def __eq__(self,other):
+    def __eq__(self, other):
         if isinstance(other, HuffmanNode):
             return self.freq == other.freq
         return False
 
-    def __lt__(self,other):
+    def __cmp__(self, other):
+        if isinstance(other, HuffmanNode):
+            return self.freq > other.freq
+
+    def __lt__(self, other):
         if isinstance(other, HuffmanNode):
             return self.freq < other.freq
 
     def __add__(self, other):
         if isinstance(other, HuffmanNode):
-            return HuffmanNode( None, self.freq+other.freq )
+            node = HuffmanNode(None, self.freq+other.freq)
+            node.left = self
+            node.right = other
+            return node
+
 
 class HuffmanTree(object):
 
+    SYMBOL_LIMIT = 257
+
     def __init__(self, text):
-        self.bst = self._initialize_bst(text)
+        self.heap = self._initialize_heap(text)
         self.root = self._construct_tree()
-        self.codes = {}
+        self.codes = self._generate_code_tree()
 
     def __repr__(self):
         return "Huffman Tree\n" + str( self.codes )
 
-    def _initialize_bst(self, text):
+    def _initialize_heap(self, byte_string):
         """
         Initializes a binary search tree with the symbols provided in text
 
@@ -44,13 +56,15 @@ class HuffmanTree(object):
         ------------
         text: iterable of text to be compressed
         """
-        bst = []
-        counter = Counter(char for char in text)
+
+        heap = []
+
+        counter = Counter(b if is_py3 else ord(b) for b in byte_string)
         for symbol, frequency in counter.items():
             node = HuffmanNode(symbol, frequency)
-            heapq.heappush(bst, node)
-        return bst
-    
+            heapq.heappush(heap, node)
+        return heap
+
     def _construct_tree(self):
         """
         Parameters
@@ -59,25 +73,78 @@ class HuffmanTree(object):
         """
         root = None
 
-        while(len(self.bst) > 1):
-            node_a, node_b = heapq.heappop(self.bst), heapq.heappop(self.bst)
+        while(len(self.heap) > 1):
+            node_a, node_b = heapq.heappop(self.heap), heapq.heappop(self.heap)
             merged_node = node_a + node_b
-            merged_node.left = node_a
-            merged_node.right = node_b
-            heapq.heappush(self.bst, merged_node)
+            heapq.heappush(self.heap, merged_node)
 
-        root = heapq.heappop(self.bst)
+        root = heapq.heappop(self.heap)
+
         return root
 
-    def generate_codes(self):
+    def _generate_code_tree(self):
         if self.root is not None:
+            codes = [0] * self.SYMBOL_LIMIT
             starting_code = ""
-            self._generate_codes( self.root, starting_code )
+            self._generate_codes(self.root, starting_code, codes)
 
-    def _generate_codes(self, current_node, current_code):
+        return codes
+
+    def _generate_codes(self, current_node, current_code, codes):
 
         if current_node is not None:
             if current_node.symbol is not None:
-                self.codes[current_node.symbol]= current_code
-            self._generate_codes(current_node.left, current_code + "0")
-            self._generate_codes(current_node.right, current_code + "1")
+                codes[current_node.symbol] = current_code
+            self._generate_codes(current_node.left, current_code+"0", codes)
+            self._generate_codes(current_node.right, current_code+"1", codes)
+
+
+class CanonicalHuffmanTree(HuffmanTree):
+
+    def __init__(self, text):
+        self.codes = self._generate_code_tree(text)
+
+    def _generate_code_tree(self,text):
+
+        naive_huff_tree = HuffmanTree(text)
+        symbol_codes = naive_huff_tree.codes
+
+        # sort symbols by code length
+        symbols_by_code_length = self._sort_symbols_by_code_length(symbol_codes)
+
+        assigned_codes = {}
+        current_code = 0
+
+        for code_length, symbols in symbols_by_code_length.items():
+            # shift the current integer by 1 bit to the left
+            current_code = current_code << 1
+
+            for sym in symbols:
+                # bin_repr = '{0:08b}'.format(current_code)
+                bin_repr = bin(current_code).lstrip("0").replace("b", "")
+                assigned_codes[sym] = bin_repr
+                current_code += 1
+        return assigned_codes
+
+    def _sort_symbols_by_code_length(self, symbol_codes):
+        d = {}
+        for symbol, code in enumerate(symbol_codes):
+            if isinstance(code, str):
+                # count number of bits in the code
+                num = int(code, 2).bit_length()
+                if d.get(num) is None:
+                    d[num] = [symbol]
+                else:
+                    d[num].append(symbol)
+        return OrderedDict(d)
+
+
+def main():
+
+    n = HuffmanNode( "a", 5)
+    n2 = HuffmanNode( "b", 3 )
+    print( n != n2)
+    print( n== n2)
+
+if __name__ == "__main__":
+    main()
